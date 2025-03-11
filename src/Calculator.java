79c92e4af478d8c1cd21514e06277e4a7537aeb2
Sungmin Jeong <sungminy1056@gmail.com>
2025-03-11 19:52:49 +0100

    "prueba 2"

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,6 +8,7 @@ public class Calculator {
 
     public int dividir(int a, int b) {
         // Error: no maneja división por 0 correctamente
+        //prudeba comentario
         return a / b;
     }
 
